bf42630c2afff2b330c3cacb1e916b197c25e983
Alec Dye <alecdye@gmail.com>
2020-02-20 12:41:07 -0800

    building context wrapper

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -6,40 +6,43 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import ProductContext from './contexts/ProductContext'
 
-import { data } from './data';
-
-// Context variable
-const StoreContext = createContext();
+import { data } from './data'
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
+	//? Logic to place item into cart
 	const addItem = item => {
 		// add the given item to the cart
+		props.addItem(item)
 	};
 
+	//? passing cart (state) into Provider
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={cart} >
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+				{/* Routes */}
+				<Route
+					exact
+					path="/"
+					render={() => (
+						<Products
+							products={products}
+							addItem={addItem}
+						/>
+					)}
+				/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+				<Route
+					path="/cart"
+					render={() => <ShoppingCart cart={cart} />}
+				/>
+			</ProductContext.Provider>
 		</div>
 	);
 }
